924cc2a8d556fbe549cea3bc73a60f6c2481c53e
Babak Mehrabi <b.mehrabi.91@gmail.com>
2025-12-18 15:30:35 +0100

    Adding open-api integration to get list of messages from API

--- src/pages/MessagesList.tsx
+++ src/pages/MessagesList.tsx
@@ -1,20 +1,32 @@
-import {Col, Table} from "antd";
+import {Alert, Col, Table} from "antd";
 import Container from "../components/container";
+import {useQuery} from "@tanstack/react-query";
+import {MessagesApi} from '../api';
+
+const api = new MessagesApi();
 
 const MessagesList = () => {
   const columns = [
-    { title: 'Title', dataIndex: 'title', key: 'title' },
-    { title: 'Created At', dataIndex: 'createdAt', key: 'createdAt' },
+    { title: 'Subject', dataIndex: 'subject', key: 'subject' },
+    {
+      title: 'Created At',
+      dataIndex: 'createdDate',
+      key: 'createdDate',
+      render: (date: string) => new Date(date).toLocaleDateString(),
+    },
   ];
-  const messages: {title: string, createdAt: string, id: number}[] = [
-    {title: 'title', id: 1, createdAt: '18.12.2025'},
-  ]
+  const { data: messages, isLoading, error } = useQuery({
+    queryKey: ['messages'] as const,
+    queryFn: () => api.findAll(),
+  });
+
+  if (error) return <Alert type="error" title={error.message} />;
 
   return (
     <Container>
       <Col>
         <h1>Messages</h1>
-        <Table dataSource={messages} columns={columns} rowKey="id" />
+        <Table loading={isLoading} dataSource={messages} columns={columns} rowKey="id" />
       </Col>
     </Container>
   )
